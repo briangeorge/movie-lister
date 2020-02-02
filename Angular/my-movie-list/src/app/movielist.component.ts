86ce7f292796ae717b4611472503016326eaf001
Brian George <brigeorge@gmail.com>
2020-02-02 06:56:55 -0500

    initial movie list component

---
+++ Angular/my-movie-list/src/app/movielist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movielist-list',
+    templateUrl: './movielist.component.html',
+    styleUrls: ['./movielist.component.css']
+})
+export class AppComponent {
+    title = 'My Movie Lists!';
+}
